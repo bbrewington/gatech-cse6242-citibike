45648dc1e448a5595782a6edc331371a38f51877
bbrewington <brent.brewington@gmail.com>
2022-03-01 23:11:13 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gatech-cse6242-citibike
+
+### Get Bike Data
+1. Run python code `create_bike_data_script.py` which creates bash script `get_bike_data.sh`
+2. Run bash script in Google Cloud terminal (probably need to make this more configurable so it doesn't do every single file):
+  ```
+  sh get_bike_data.sh
+  ```
---
+++ get_bike_data.sh
@@ -0,0 +1,299 @@
+curl https://s3.amazonaws.com/tripdata/201807-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201807-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201807-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201807-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201807-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201808-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201808-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201808-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201808-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201808-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201809-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201809-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201809-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201809-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201809-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201810-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201810-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201810-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201810-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201810-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201811-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201811-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201811-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201811-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201811-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201812-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201812-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201812-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201812-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201812-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201901-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201901-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201901-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201901-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201901-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201902-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201902-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201902-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201902-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201902-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201903-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201903-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201903-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201903-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201903-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201904-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201904-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201904-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201904-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201904-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201905-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201905-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201905-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201905-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201905-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201906-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201906-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201906-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201906-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201906-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201907-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201907-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201907-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201907-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201907-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201908-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201908-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201908-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201908-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201908-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201909-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201909-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201909-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201909-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201909-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201910-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201910-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201910-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201910-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201910-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201911-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201911-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201911-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201911-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201911-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/201912-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/201912-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/201912-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/201912-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/201912-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202001-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202001-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202001-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202001-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202001-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202002-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202002-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202002-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202002-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202002-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202003-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202003-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202003-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202003-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202003-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202004-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202004-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202004-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202004-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202004-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202005-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202005-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202005-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202005-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202005-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202006-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202006-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202006-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202006-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202006-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202007-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202007-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202007-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202007-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202007-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202008-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202008-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202008-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202008-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202008-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202009-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202009-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202009-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202009-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202009-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202010-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202010-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202010-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202010-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202010-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202011-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202011-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202011-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202011-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202011-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202012-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202012-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202012-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202012-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202012-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202101-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202101-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202101-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202101-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202101-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202102-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202102-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202102-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202102-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202102-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202103-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202103-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202103-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202103-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202103-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202104-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202104-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202104-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202104-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202104-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202105-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202105-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202105-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202105-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202105-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202106-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202106-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202106-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202106-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202106-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202107-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202107-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202107-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202107-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202107-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202108-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202108-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202108-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202108-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202108-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202109-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202109-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202109-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202109-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202109-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202110-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202110-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202110-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202110-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202110-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202111-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202111-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202111-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202111-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202111-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202112-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202112-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202112-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202112-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202112-citibike-tripdata.csv.zip
+
+
+curl https://s3.amazonaws.com/tripdata/202201-citibike-tripdata.csv.zip | gsutil cp - gs://citibike_trip_history/202201-citibike-tripdata.csv.zip
+
+gsutil cat gs://citibike_trip_history/202201-citibike-tripdata.csv.zip | zcat | gsutil cp - gs://citibike_trip_history/202201-citibike-tripdata.csv
+
+gsutil rm gs://citibike_trip_history/202201-citibike-tripdata.csv.zip

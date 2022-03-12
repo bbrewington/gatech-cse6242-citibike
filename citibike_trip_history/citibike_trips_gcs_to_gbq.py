96c645a608914256b6ebe35c27dba2a6aa2884af
bbrewington <brent.brewington@gmail.com>
2022-03-12 13:44:55 -0500

    update load script

--- citibike_trip_history/citibike_trips_gcs_to_gbq.py
+++ citibike_trip_history/citibike_trips_gcs_to_gbq.py
@@ -1,4 +1,39 @@
-from google.cloud import bigquery
+from google.cloud import storage, bigquery
+
+source_bucket = 'citibike_trip_history'
+dest_dataset = 'STG5'
+
+def gcs_list_blobs(project_id, bucket_name):
+    storage_client = storage.Client(project_id)
+    blobs = storage_client.list_blobs(bucket_name)
+
+    return [blob.name for blob in blobs]
+
+exclude_list = ['201806-citibike-tripdata.csv', '202102-citibike-tripdata.csv'] # filenames already loaded
+filelist1 = gcs_list_blobs('cse-6242-sp22-nyatl', 'citibike_trip_history')
+filelist2 = sorted([x for x in filelist1 if x not in exclude_list])
+
+def gcs_to_bigquery(project_id, source_bucket, source_blob, dest_dataset, dest_table, dest_schema):
+    client = bigquery.Client(project_id)
+
+    table_id = project_id + '.' + dest_dataset + '.' + dest_table
+
+    job_config = bigquery.LoadJobConfig(
+        schema=dest_schema,
+        skip_leading_rows=1,
+        source_format = bigquery.SourceFormat.CSV,
+        write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+    )
+
+    uri = "gs://" + source_bucket + '/' + source_blob
+
+    print('loading ' + table_id)
+
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )
+
+    load_job.result()
 
 gbq_schema_201806_202101 = [
     bigquery.SchemaField("tripduration", "STRING"),
@@ -34,24 +69,14 @@ gbq_schema_202102_onward = [
     bigquery.SchemaField("member_casual", "STRING")
 ]
 
-def gcs_to_bigquery(project_id, source_bucket, source_blob, dest_dataset, dest_table, dest_schema):
-    client = bigquery.Client(project_id)
+for filename in filelist2:
+    print(filename)
+    month_key = filename[:6]
+    if month_key < '202102':
+        dest_schema = gbq_schema_201806_202101
+        dest_table = 'citibike_trip_201806_202101'
+    elif month_key >= '202102':
+        dest_schema = gbq_schema_202102_onward
+        dest_table = 'citibike_trip_202102_onward'
 
-    table_id = project_id + '.' + dest_dataset + '.' + dest_table
-
-    job_config = bigquery.LoadJobConfig(
-        schema=dest_schema,
-        skip_leading_rows=1,
-        source_format = bigquery.SourceFormat.CSV,
-        write_disposition = bigquery.WriteDisposition.WRITE_APPEND
-    )
-
-    uri = "gs://" + source_bucket + '/' + source_blob
-
-    load_job = client.load_table_from_uri(
-        uri, table_id, job_config=job_config
-    )
-
-    load_job.result()
-
-gcs_to_bigquery(project_id='cse-6242-sp22-nyatl', source_bucket='citibike_trip_history', source_blob='202102-citibike-tripdata.csv', dest_dataset='STAGING_EXP_24HR', dest_table='citibike_trip_202102_onward', dest_schema=gbq_schema_202102_onward)
+    gcs_to_bigquery(project_id='cse-6242-sp22-nyatl', source_bucket=source_bucket, source_blob=filename, dest_dataset=dest_dataset, dest_table=dest_table, dest_schema=dest_schema)

9a7ce26bbb2f1587b589469bdee37f966d1f7398
bbrewington <brent.brewington@gmail.com>
2022-03-19 11:35:10 -0400

    patch in 201610 to 201703

--- citibike_trip_history/citibike_views.sql
+++ citibike_trip_history/citibike_views.sql
@@ -1,3 +1,29 @@
+create or replace view `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip_201610_201703`
+  options(description = 'view definition tracked in github: https://github.com/bbrewington/gatech-cse6242-citibike/tree/main/citibike_trip_history')
+  as
+
+  select
+    -- Date/Time, Station Info, Lat/Lon
+      datetime(starttime) as started_at
+    , datetime(stoptime) as ended_at
+    , cast(start_station_id as string) as start_station_id
+    , cast(start_station_name as string) as start_station_name
+    , cast(start_station_latitude as float64) as start_lat
+    , cast(start_station_longitude as float64) as start_lng
+    , cast(end_station_id as string) as end_station_id
+    , cast(end_station_name as string) as end_station_name
+    , cast(end_station_latitude as float64) as end_lat
+    , cast(end_station_longitude as float64) as end_lng
+
+    -- Other Attributes
+    , datetime_diff(datetime(stoptime), datetime(starttime), minute) as trip_duration_mins
+    , cast(bikeid as int64) as bike_id
+    , cast(usertype as string) as user_type -- values: Subscriber, Consumer
+    , cast(birth_year as int64) as birth_year -- 4 digit year
+    , cast(gender as string) as gender -- values: 0, 1, 2
+  from `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip_201610_201703_raw`
+;
+
 create or replace view `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip_201806_202101`
   options(description = 'view definition tracked in github: https://github.com/bbrewington/gatech-cse6242-citibike/tree/main/citibike_trip_history')
   as
@@ -101,6 +127,30 @@ create or replace view `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip`
     , cast(null as string) as ride_id
     , 'bq_public' as source
   from `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip_bq_public`
+
+  union all
+
+  select
+      started_at
+    , ended_at
+    , start_station_id
+    , start_station_name
+    , start_lat
+    , start_lng
+    , end_station_id
+    , end_station_name
+    , end_lat
+    , end_lng
+    , trip_duration_mins
+    , bike_id
+    , user_type
+    , birth_year
+    , gender
+    , cast(null as string) as rideable_type
+    , cast(null as string) as member_casual
+    , cast(null as string) as ride_id
+    , 'aws_201610_201703' as source
+  from `cse-6242-sp22-nyatl.CITIBIKE.citibike_trip_201610_201703`
 
   union all
 

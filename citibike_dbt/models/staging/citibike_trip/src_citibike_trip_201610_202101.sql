f4c11c149fd39a538c7eaa214ae672a9ec37a314
bbrewington <brent.brewington@gmail.com>
2022-04-11 00:18:48 -0400

    update table union

--- citibike_dbt/models/staging/citibike_trip/src_citibike_trip_201610_202101.sql
+++ citibike_dbt/models/staging/citibike_trip/src_citibike_trip_201610_202101.sql
@@ -18,8 +18,65 @@ with source as (
         , cast(usertype as string) as user_type -- values: Subscriber, Consumer
         , cast(birth_year as int64) as birth_year -- 4 digit year
         , cast(gender as string) as gender -- values: 0, 1, 2
-    from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_*`
-    where _TABLE_SUFFIX between '20161001' and '20210101'
+    from (
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20161001` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20161101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20161201` union all
+
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170201` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170301` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170401` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170501` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170601` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170701` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170801` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20170901` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20171001` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20171101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20171201` union all
+
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180201` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180301` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180401` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180501` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180601` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180701` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180801` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20180901` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20181001` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20181101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20181201` union all
+
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190201` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190301` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190401` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190501` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190601` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190701` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190801` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20190901` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20191001` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20191101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20191201` union all
+
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200201` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200301` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200401` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200501` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200601` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200701` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200801` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20200901` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20201001` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20201101` union all
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20201201` union all
+
+        select * from `cse-6242-sp22-nyatl.CITIBIKE_STG.citibike_trip_20210101`
+    )
 )
 
 select * from source

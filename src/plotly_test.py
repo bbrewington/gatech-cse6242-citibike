2fa268e397fac83d4513c3bbfbcebec5bfb536bc
bbrewington <brent.brewington@gmail.com>
2022-04-12 23:25:16 -0400

    update plotly example

---
+++ src/plotly_test.py
@@ -0,0 +1,18 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+
+df = pd.DataFrame({
+  "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+  "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+  "Number Eaten": [2, 1, 3, 1, 3, 2],
+})
+
+fig = go.Figure()
+for contestant, group in df.groupby("Contestant"):
+    fig.add_trace(go.Bar(x=group["Fruit"], y=group["Number Eaten"], name=contestant,
+      hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"% contestant))
+fig.update_layout(legend_title_text = "Contestant")
+fig.update_xaxes(title_text="Fruit")
+fig.update_yaxes(title_text="Number Eaten")
+fig.write_html('plotly_example.html', include_plotlyjs='cdn')
